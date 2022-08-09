9087e9772d27d12448d1006809766d5d76c05577
Fernando Mumbach <fermuch@cryptolab.net>
2022-08-09 18:14:17 +0000

    near beacon ideas

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,22 @@
 import * as MonoUtils from "@fermuch/monoutils";
 import type { BeaconData } from "react-native-beacon-scanner";
 
-// based on settingsSchema @ package.json
-type Config = Record<string, unknown> & {}
-
-const conf = new MonoUtils.config.Config<Config>();
-
 declare class BeaconScanEvent extends MonoUtils.wk.event.BaseEvent {
   kind: 'beacon-scan-event';
   beacons: BeaconData[];
 
   getData(): {beacons: BeaconData[]};
 }
+
+type Config = Record<string, unknown> & {}
+const conf = new MonoUtils.config.Config<Config>();
+
+interface NearBeacon {
+  mac: string;
+  rssi: number;
+  lastUpdate: number;
+};
+const nearBeacons: NearBeacon[] = [];
 
 MonoUtils.wk.event.subscribe<BeaconScanEvent>('beacon-scan-event', (ev) => {
   env.project?.saveEvent(ev);
