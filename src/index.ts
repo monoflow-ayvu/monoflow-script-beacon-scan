ad9ef43247fbdf87d0bdfdc6a5f3e6357dc2e82e
Fernando Mumbach <fermuch@cryptolab.net>
2022-08-10 14:47:09 +0000

    publishing CLOSES_IBEACON

--- src/index.ts
+++ src/index.ts
@@ -8,16 +8,41 @@ declare class BeaconScanEvent extends MonoUtils.wk.event.BaseEvent {
   getData(): {beacons: BeaconData[]};
 }
 
-type Config = Record<string, unknown> & {}
+type ConfigBeaconCheck = {
+  enableBeaconCheck: boolean;
+  tags?: string[];
+  pageId?: string;
+}
+
+type Config =
+  ConfigBeaconCheck
+  & {};
+
 const conf = new MonoUtils.config.Config<Config>();
 
 interface NearBeacon {
+  name?: string;
   mac: string;
-  rssi: number;
-  lastUpdate: number;
+  distance: number;
 };
-const nearBeacons: NearBeacon[] = [];
 
 MonoUtils.wk.event.subscribe<BeaconScanEvent>('beacon-scan-event', (ev) => {
   env.project?.saveEvent(ev);
+
+  if (!conf.get('enableBeaconCheck', false)) {
+    return;
+  }
+
+  const nearBeacons: NearBeacon[] = ev.beacons
+    .filter((b) => b.frames.some((f) => f.type === 'ibeacon'))
+    .map((b) => ({
+      mac: b.mac,
+      distance: 0,
+    }))
+  const closestBeacon = nearBeacons.sort((a, b) => a.distance - b.distance)?.[0] ?? [];
+  if (!closestBeacon) {
+    return;
+  }
+
+  env.setData('CLOSEST_IBEACON', closestBeacon);
 });
